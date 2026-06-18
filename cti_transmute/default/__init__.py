0a3c301b9a4244685e048ef7e201779a0eea3329
Christian Studer <christian.studer@circl.lu>
2026-06-18 13:23:13 +0200

    fix: [cti_transmute] Defining `env_global_name` before helpers import to avoid circular import

--- cti_transmute/default/__init__.py
+++ cti_transmute/default/__init__.py
@@ -1,9 +1,9 @@
 import os
 
+env_global_name: str = 'TRANSMUTE_HOME'
+
 from .exceptions import ConfigError, MissingEnv
 from .helpers import get_config, get_homedir, load_configs
-
-env_global_name: str = 'TRANSMUTE_HOME'
 
 os.chdir(get_homedir())
 
