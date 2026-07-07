1ea1ff6c98fa911cbc6112eb6a1412334064e071
Christian Studer <christian.studer@circl.lu>
2026-07-07 14:13:09 +0200

    fix: [bin] Using renamed variables to register blueprints

--- bin/start_website.py
+++ bin/start_website.py
@@ -23,13 +23,14 @@ def main():
 
 
     from website.web.account.account import account_blueprint
-    from website.web.convert.convert import convert_blueprint, legacy_convert_blueprint
+    from website.web.conversions.conversions import (
+        conversions_blueprint, legacy_convert_blueprint)
     from website.web.evaluate.evaluate import evaluate_blueprint
     from website.web.home import home_blueprint
     from website.web.tags.tags import tags_blueprint
 
     application.register_blueprint(home_blueprint, url_prefix="/")
-    application.register_blueprint(convert_blueprint, url_prefix="/conversions")
+    application.register_blueprint(conversions_blueprint, url_prefix="/conversions")
     application.register_blueprint(legacy_convert_blueprint, url_prefix="/convert")
     application.register_blueprint(account_blueprint, url_prefix="/account")
     application.register_blueprint(tags_blueprint, url_prefix="/tags")
